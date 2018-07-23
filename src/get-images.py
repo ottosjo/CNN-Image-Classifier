981b6478d42eed4840ce60baaef260fe02040cfe
Otto Sjöholm <ottosjhlm@gmail.com>
2018-07-23 17:14:35 +0200

    Update to enable fetching of images using the 'similar_images' keyword

--- src/get-images.py
+++ src/get-images.py
@@ -4,15 +4,23 @@ from google_images_download import google_images_download
 
 response = google_images_download.googleimagesdownload()
 categories = ["poodle", "pizza"]
+similar = [
+    "https://s3.amazonaws.com/playbarkrun/wp-content/uploads/2018/05/28100132/best-poodle-clippers1.jpg",
+    "https://img.koket.se/media/pizza-med-rodlok-och-bacon.jpg"
+]
 arguments={
     "limit": 100,
+    "format": "jpg",
     "size":"medium",
-    "output_directory": "./images"
+    #"output_directory": "./images/training",
+    "output_directory": "./images/validation",
 }
 
 if (arguments["limit"]>100):
     raise ValueError('the argument "limit" must not be greater than 100') 
 
-for category in categories:
-    arguments["keywords"] = category
+for i in range(len(categories)):
+    arguments["keywords"] = categories[i]
+    arguments["similar_images"] = similar[i]
+
     response.download(arguments)
