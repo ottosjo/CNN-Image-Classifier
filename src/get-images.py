62e3019ef4719edfa312845acfadc06fd7d3de23
Otto Sjöholm <ottosjhlm@gmail.com>
2018-07-25 01:33:10 +0200

    Improve code for image download.

--- src/get-images.py
+++ src/get-images.py
@@ -3,32 +3,44 @@ from google_images_download import google_images_download
 # conf_file = '../conf/get-images.json'
 
 response = google_images_download.googleimagesdownload()
+image_base_dir = "./images/"
 
+image_types = [
+    "training",
+    "validation",
+    "test"
+]
+image_counts = [
+    1000,
+    300,
+    100
+]
 categories = [
-    #"poodle",
-    #"pizza",
-    "plane"
+    "flower",
+    "sausage",
+    "wolf"
     ]
-
 similar = [
-    #"https://s3.amazonaws.com/playbarkrun/wp-content/uploads/2018/05/28100132/best-poodle-clippers1.jpg",
-    #"https://img.koket.se/media/pizza-med-rodlok-och-bacon.jpg",
-    "http://www.slate.com/content/dam/slate/articles/health_and_science/explainer/2012/06/120614_EX_Airplane.jpg.CROP.rectangle3-large.jpg"
+    
     ]
 
 arguments={
-    "limit":            100,
     "format":           "jpg",
     "size":             "medium",
-    #"output_directory": "./images/training",
-    "output_directory": "./images/validation",
+    #"limit":            100,
+    #"output_directory": "./images/...",
 }
 
-if (arguments["limit"]>100):
-    raise ValueError('the argument "limit" must not be greater than 100') 
+for i in range(3):
+    im_type = image_types[i]
+    im_count = image_counts[i]
 
-for i in range(len(categories)):
-    arguments["keywords"] = categories[i]
-    arguments["similar_images"] = similar[i]
+    print("Downloading images")
+    arguments["limit"] = im_count
+    arguments["output_directory"] = image_base_dir + im_type
 
-    response.download(arguments)
+    for i in range(len(categories)):
+        arguments["keywords"] = categories[i]
+        #arguments["similar_images"] = similar[i]
+        response.download(arguments)
+
