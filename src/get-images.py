fa39c3196ca607930e7337f138aaa9cae037e9ed
Otto Sjöholm <ottosjhlm@gmail.com>
2018-07-25 01:00:35 +0200

    Add plane category when fetching images.

--- src/get-images.py
+++ src/get-images.py
@@ -3,15 +3,23 @@ from google_images_download import google_images_download
 # conf_file = '../conf/get-images.json'
 
 response = google_images_download.googleimagesdownload()
-categories = ["poodle", "pizza"]
+
+categories = [
+    #"poodle",
+    #"pizza",
+    "plane"
+    ]
+
 similar = [
-    "https://s3.amazonaws.com/playbarkrun/wp-content/uploads/2018/05/28100132/best-poodle-clippers1.jpg",
-    "https://img.koket.se/media/pizza-med-rodlok-och-bacon.jpg"
-]
+    #"https://s3.amazonaws.com/playbarkrun/wp-content/uploads/2018/05/28100132/best-poodle-clippers1.jpg",
+    #"https://img.koket.se/media/pizza-med-rodlok-och-bacon.jpg",
+    "http://www.slate.com/content/dam/slate/articles/health_and_science/explainer/2012/06/120614_EX_Airplane.jpg.CROP.rectangle3-large.jpg"
+    ]
+
 arguments={
-    "limit": 100,
-    "format": "jpg",
-    "size":"medium",
+    "limit":            100,
+    "format":           "jpg",
+    "size":             "medium",
     #"output_directory": "./images/training",
     "output_directory": "./images/validation",
 }
